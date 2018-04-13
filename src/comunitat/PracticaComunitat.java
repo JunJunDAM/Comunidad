d14d926da4577f141b6d37a9467888a3b596014f
Jun <juninfo1997@gmail.com>
2018-04-13 17:45:02 +0200

    Read Despeses OK

--- src/comunitat/PracticaComunitat.java
+++ src/comunitat/PracticaComunitat.java
@@ -8,6 +8,7 @@ package comunitat;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -23,12 +24,11 @@ public class PracticaComunitat {
      * @param args the command line arguments
      */
     //Array de informacion de los txt
-    static List<String> lineasComunidad = new ArrayList<>();
-    static List<String> lineasDespeses = new ArrayList<>();
-    //Array de 
-    static List<Comunitat> listaComunidades = new ArrayList<>();
-    static List<Despeses> listaDespeses = new ArrayList<>();
+    static List<String> comunidadTXT = new ArrayList<>();
+    static List<String> despesesTXT = new ArrayList<>();
+
     //Array de clases
+    static List<Despeses> listaDespeses = new ArrayList<>();
     static List<Comunitat> listaComunidad = new ArrayList<>();
     static List<Zona> listaZona = new ArrayList<>();
     static List<Propietat> listaPropiedad = new ArrayList<>();
@@ -38,60 +38,178 @@ public class PracticaComunitat {
         try {
             Scanner com = new Scanner(new FileReader("Comunitats.txt"));
             Scanner des = new Scanner(new FileReader("Despeses.txt"));
-
+            //Añadimos a las array de lineas todo el contenido de los txt
             while (com.hasNext()) {
-                lineasComunidad.add(com.nextLine());
+                comunidadTXT.add(com.nextLine());
+            }
+            while (des.hasNext()) {
+                despesesTXT.add(des.nextLine());
             }
 
-            while (des.hasNext()) {
-                lineasDespeses.add(des.nextLine());
-            }
-            
             limpieza();
-            
-            System.out.println("*********** lista comunidad");
-            System.out.println(listaComunidad);
-            System.out.println("*********** lista despeses");
-            System.out.println(lineasDespeses);
-            /*for (int i = 0; i < lineasComunidad.size(); i++) {
-                procesar(i);
-            }*/
 
         } catch (FileNotFoundException ex) {
             Logger.getLogger(PracticaComunitat.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public static void callateya(){
-        
-    }
-    
     public static void limpieza() {
-        List<String> lista = new ArrayList<>();
-        List<String> lista2 = new ArrayList<>();
+        List<String> lista1 = new ArrayList<>();
+        List lista2 = new ArrayList();
+        List lista3 = new ArrayList();
+        List lista4 = new ArrayList();
 
-        for (int i = 0; i < lineasComunidad.size(); i++) {
-            String[] datos = lineasComunidad.get(i).split("#");
-            lista.add(datos[0]);
+        HashMap<String, String> comunidadHS = new HashMap<>();
+        System.out.println(comunidadTXT.size());
+
+        String[] datos = null;
+        
+        //Borro los espacios vacios del Comunitats.txt
+        for (int i = 0; i < comunidadTXT.size(); i++) {
+            if (comunidadTXT.get(i).isEmpty()) {
+                datos = comunidadTXT.get(i).split(" ");
+            }
+        }
+        System.out.println("***");
+        System.out.println(datos[1]);
+        System.out.println("***");
+        
+        for (int i = 0; i < comunidadTXT.size(); i++) {
+            
+            String line = comunidadTXT.get(i);
+            
+            
+           /* if (comunidadTXT.get(i).equalsIgnoreCase("#Comunitat")) {
+                String p = comunidadTXT.get(i);
+                char pp = p.charAt(0);
+                String ppp = String.valueOf(pp);
+                while (!ppp.equalsIgnoreCase("#Zona")) {
+                    lista1.add(comunidadTXT.get(i));
+                    
+                }
+            }
+
+            if (comunidadTXT.get(i).equalsIgnoreCase("#Zona")) {
+                comunidadTXT.remove(comunidadTXT.get(i));
+                String p = comunidadTXT.get(i);
+                char pp = p.charAt(0);
+                String ppp = String.valueOf(pp);
+                while (!ppp.equalsIgnoreCase("#")) {
+                    lista2.add(comunidadTXT.get(i));
+                }
+            }
+
+            if (comunidadTXT.get(i).equalsIgnoreCase("#Propietat")) {
+                comunidadTXT.remove(comunidadTXT.get(i));
+                String p = comunidadTXT.get(i);
+                char pp = p.charAt(0);
+                String ppp = String.valueOf(pp);
+                while (!ppp.equalsIgnoreCase("#")) {
+                    lista3.add(comunidadTXT.get(i));
+                }
+            }
+
+            if (comunidadTXT.get(i).equalsIgnoreCase("#Propietari")) {
+                comunidadTXT.remove(comunidadTXT.get(i));
+                String p = comunidadTXT.get(i);
+                char pp = p.charAt(0);
+                String ppp = String.valueOf(pp);
+                while (!ppp.equalsIgnoreCase("#")) {
+                    lista4.add(comunidadTXT.get(i));
+                }
+            }*/
+
         }
 
-        
-        
-        for (int i = 0; i < lista.size(); i++) {
-            String[] datos = lista.get(i).split(" ");
+        System.out.println("Comunitat : " + lista1.size());
+        System.out.println(lista1);
+        System.out.println("Zona : " + lista2.size());
+        System.out.println(lista2);
+        System.out.println("Propietat : " + lista3.size());
+        System.out.println(lista3);
+        System.out.println("Propietari : " + lista4.size());
+        System.out.println(lista4);
 
-            
-            String identificacio = datos[0];
+        /*for (int i = 0; i < comunidadTXT.size(); i++) {
+            //Cojo el primer caracter de cada linea
+            String p = comunidadTXT.get(i);
+            char pp = p.charAt(0);
+            String ppp = String.valueOf(pp);*/
+ /*if (ppp.equalsIgnoreCase("#")) {*/
+        //String[] datos = comunidadTXT.get(i).split("#");
+        //lista1.add(comunidadTXT.get(i));
+        /*for(int i2 = 0; i2 < lista1.size(); i2++){
+                    System.out.println(lista1);
+                }*/
+ /*String comunidad = datos[0];
+                lista1.add(comunidad);
+                System.out.println(lista1);
+                System.out.println("0");
+                
+                String zona = datos[1];
+                lista2.add(zona);
+                System.out.println(lista2);
+                System.out.println("1");
+                
+                String propiedad = datos[2];
+                lista3.add(propiedad);
+                System.out.println(propiedad);
+                System.out.println("2");
+                
+                String propietari = datos[3];
+                lista4.add(propietari);
+                System.out.println(lista4);
+                System.out.println("3");*/
+ /*}*/
+    }
+
+    /*
+        String[] datos = null;
+        String[] datos2 = null;
+
+        //Separo y añado en lista por #
+        for (int i = 0; i < comunidadTXT.size(); i++) {
+            datos = comunidadTXT.get(i).split("#");
+            lista1.add(datos[0]);
+        }
+        //System.out.println(lista1);
+        datos2 = lista1.get(0).split(" ");
+        System.out.println(datos2[0]);
+     */
+ /*lista1.add(datos[0]);
+        System.out.println(lista1);*/
+
+ /*for (int i = 0; i < comunidadTXT.size(); i++) {
+            String p = comunidadTXT.get(i);
+            char pp = p.charAt(0);
+            String ppp = String.valueOf(pp);
+            if (ppp.equalsIgnoreCase("#")) {
+                System.out.println(comunidadTXT.get(i));
+                comunidadTXT.remove(comunidadTXT.get(i));
+            } else if (ppp.equalsIgnoreCase(".")) {
+                System.out.println(comunidadTXT.get(i));
+                comunidadTXT.remove(comunidadTXT.get(i));
+            } else {
+                lista2.add(comunidadTXT.get(i));
+                
+            }
+        }*/
+    //System.out.println(lista2);
+    /*String[] datos2 = lista.get(0).split(" ");
+            for(int i = 0; i < datos.length;i++){
+                System.out.println(datos[0]);
+            }*/
+
+ /*String identificacio = datos[0];
             String nom = datos[1];
             String poblacio = datos[2];
 
             Comunitat comunitat = new Comunitat(identificacio, nom, poblacio);
-            listaComunidad.add(comunitat);
-        }
-    }
+            listaComunidad.add(comunitat);*/
+    //}
 
-    public static void procesar(int i) {
-        String[] datos = lineasComunidad.get(i).split(";");
+    /*public static void procesar(int i) {
+        String[] datos = comunidadTXT.get(i).split(";");
         String identificacio = datos[0];
         String nom = datos[1];
         String poblacio = datos[2];
@@ -99,21 +217,21 @@ public class PracticaComunitat {
         Comunitat comunidad = new Comunitat(identificacio, nom, poblacio);
         listaComunidades.add(comunidad);
         System.out.println(identificacio + " " + nom + " " + poblacio);
-    }
-
-    
-    
-    public static void prueba() {
-        for (int i = 0; i < lineasDespeses.size(); i++) {
-            String[] datos = lineasDespeses.get(i).split(";");
-            String p = lineasDespeses.get(i);
+    }*/
+    public static void cleanDespeses() {
+        System.out.println("******* Lista despeses: ");
+        for (int i = 0; i < despesesTXT.size(); i++) {
+            //Cojo el primer caracter de cada linea
+            String p = despesesTXT.get(i);
             char pp = p.charAt(0);
             String ppp = String.valueOf(pp);
-
+            //
             if (ppp.equalsIgnoreCase("#")) {
-                lineasDespeses.remove(lineasDespeses.get(i));
+                despesesTXT.remove(despesesTXT.get(i));
+            } else if (ppp.equalsIgnoreCase(".")) {
+                despesesTXT.remove(despesesTXT.get(i));
             } else {
-
+                String[] datos = despesesTXT.get(i).split(";");
                 String identificacio = datos[0];
                 String descripcio = datos[1];
                 Double importe = Double.valueOf(datos[2]);
@@ -123,7 +241,7 @@ public class PracticaComunitat {
                 listaDespeses.add(despeses);
             }
         }
-
+        System.out.println(listaDespeses);
     }
 
 }
